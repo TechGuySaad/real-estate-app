0d7e2d557162a193d3ab80c28872467845ae621f
Saad Rahman <saadreh5@gmail.com>
2025-10-26 17:46:01 +0500

    creating splash screen

---
+++ app/(onboarding)/splash.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Splash = () => {
+  return (
+    <View className="h-full flex flex-col items-center justify-center">
+      <Text className="w-20">Splash</Text>
+    </View>
+  );
+};
+
+export default Splash;
