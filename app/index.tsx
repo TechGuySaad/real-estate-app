637252ac238f30fde06e4cd5366e03154458d341
Saad Rahman <saadreh5@gmail.com>
2025-10-26 17:43:05 +0500

    added main files

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import "../global.css";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-black">
+    <View className="flex  justify-center items-center h-full ">
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
